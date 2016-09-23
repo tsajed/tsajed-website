404e5c8d69d78907330e6717b8af4e9064ae69fd
Tanvir Sajed <tsajed@ualberta.ca>
2016-09-23 14:42:41 -0600

    Add resume in word format

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,9 @@ Rails.application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
+  
+  # Display pdf files
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
